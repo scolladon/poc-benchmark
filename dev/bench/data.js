cad278281203e966b1b099d7da7dabd973968b21
github-action-benchmark <github@users.noreply.github.com>
2023-07-21 15:40:11 +0000

    add Benchmark.js Benchmark (benchmarkjs) benchmark result for bfe9e1a010cd6fbaf8318e26fe6a510480ffe399

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1689953850123,
+  "lastUpdate": 1689954011869,
   "repoUrl": "https://github.com/scolladon/poc-benchmark",
   "entries": {
     "Benchmark.js Benchmark": [
@@ -90,6 +90,51 @@ window.BENCHMARK_DATA = {
             "range": "±1.09%",
             "unit": "ops/sec",
             "extra": "67 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "colladonsebastien@gmail.com",
+            "name": "Sébastien Colladon",
+            "username": "scolladon"
+          },
+          "committer": {
+            "email": "colladonsebastien@gmail.com",
+            "name": "Sébastien Colladon",
+            "username": "scolladon"
+          },
+          "distinct": true,
+          "id": "bfe9e1a010cd6fbaf8318e26fe6a510480ffe399",
+          "message": "feat: slow down a bit",
+          "timestamp": "2023-07-21T17:39:28+02:00",
+          "tree_id": "c8cad39c54c94d97ece938a1f2eea37699819d01",
+          "url": "https://github.com/scolladon/poc-benchmark/commit/bfe9e1a010cd6fbaf8318e26fe6a510480ffe399"
+        },
+        "date": 1689954011064,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "fib(10)",
+            "value": 568506,
+            "range": "±1.05%",
+            "unit": "ops/sec",
+            "extra": "90 samples"
+          },
+          {
+            "name": "fib(20)",
+            "value": 4933,
+            "range": "±0.94%",
+            "unit": "ops/sec",
+            "extra": "90 samples"
+          },
+          {
+            "name": "fib(30)",
+            "value": 40.5,
+            "range": "±0.62%",
+            "unit": "ops/sec",
+            "extra": "53 samples"
           }
         ]
       }
