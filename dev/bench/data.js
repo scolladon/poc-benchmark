dc0f43a142bdbb28f2102c6c27bf94e5be133c98
github-action-benchmark <github@users.noreply.github.com>
2023-07-21 15:37:30 +0000

    add Benchmark.js Benchmark (benchmarkjs) benchmark result for 3f31c1e3c1ef417f2222de91c343c06edb6e6f7a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,8 +1,53 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1689953848726,
+  "lastUpdate": 1689953850123,
   "repoUrl": "https://github.com/scolladon/poc-benchmark",
   "entries": {
     "Benchmark.js Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "colladonsebastien@gmail.com",
+            "name": "Sébastien Colladon",
+            "username": "scolladon"
+          },
+          "committer": {
+            "email": "colladonsebastien@gmail.com",
+            "name": "Sébastien Colladon",
+            "username": "scolladon"
+          },
+          "distinct": true,
+          "id": "3f31c1e3c1ef417f2222de91c343c06edb6e6f7a",
+          "message": "fix: gh action",
+          "timestamp": "2023-07-21T17:36:32+02:00",
+          "tree_id": "ef6863675039b818c09629faa339441ecfbd62bf",
+          "url": "https://github.com/scolladon/poc-benchmark/commit/3f31c1e3c1ef417f2222de91c343c06edb6e6f7a"
+        },
+        "date": 1689953847982,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "fib(10)",
+            "value": 952119,
+            "range": "±0.74%",
+            "unit": "ops/sec",
+            "extra": "89 samples"
+          },
+          {
+            "name": "fib(20)",
+            "value": 8173,
+            "range": "±0.71%",
+            "unit": "ops/sec",
+            "extra": "86 samples"
+          },
+          {
+            "name": "fib(30)",
+            "value": 66.08,
+            "range": "±1.09%",
+            "unit": "ops/sec",
+            "extra": "67 samples"
+          }
+        ]
+      },
       {
         "commit": {
           "author": {
